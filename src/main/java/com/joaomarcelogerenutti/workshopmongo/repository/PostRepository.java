2a1439c6ef1dcc4496a98037faba622f9b866dbb
João Marcelo Gerenutti <joaomarcelogerenuttii@hotmail.com>
2022-09-26 16:16:18 -0300

    Simple query with @Query.

--- src/main/java/com/joaomarcelogerenutti/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/joaomarcelogerenutti/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.joaomarcelogerenutti.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.joaomarcelogerenutti.workshopmongo.domain.Post;
@@ -10,5 +11,9 @@ import com.joaomarcelogerenutti.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	// Learning-only method.
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchByTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
